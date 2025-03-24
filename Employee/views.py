bd3701da0e4908f519ce455c06a64bb316da3223
Anish Prakash I <anishprakash149@gmail.com>
2025-03-24 17:58:24 +0530

    Clean up unwanted files

---
+++ Employee/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from Employee.models import Employee
+
+# Create your views here.
+
+def employee_details (request,pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        'employee': employee,
+    }
+    return render(request, 'employee_detail.html', context)
---
+++ mysite/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from Employee.models import Employee
+
+def home(request):
+    employees = Employee.objects.all()
+    context = {
+        'employees': employees,
+    }
+    
+    return render(request, 'home.html', context)
